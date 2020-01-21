b3f14bf9a3c328a9e0ea454e05b60076f8605825
ziyaadm <ziyaadm@gmail.com>
2020-01-21 20:44:56 +0000

    updated page name

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Header(props) {
   return (
     <nav className="navbar navbar-dark bg-dark">
-      $ Wicked Sales
+      $ ZlickDeals
       <button type="button" className="btn btn-primary fas fa-shopping-cart" onClick={() => props.setView('cart', {})}>
         <span className="badge badge-light">{props.cartItemCount}</span>
       </button>
